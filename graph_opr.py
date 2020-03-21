23c59f50bb2b62f399810fe9c1041f3b84a90345
hjwoohakim <hjwoohakim@163.com>
2020-03-22 00:11:35 +0800

    change draw

--- graph_opr.py
+++ graph_opr.py
@@ -1,16 +1,14 @@
 import networkx as nx
-import pygraphviz as pgv
-from nxpd import draw, nxpdParams
-nxpdParams['show'] = 'ipynb'
+import matplotlib.pyplot as plt
 
 # create a graph
 G = nx.Graph()
 # this graph has two connected components
 G.add_edges_from([('a', 'b'), ('b', 'c'), ('c', 'd'), ('e', 'f')])
 # draw this graph
-draw(G, layout='circo')
+nx.draw(G)
 print('this graph has ', nx.number_connected_components(G), 'connected components')
 # print each connected components
-for comp in nx.connected_components:
+for comp in nx.connected_components(G):
     print(comp)
-
+plt.show()
